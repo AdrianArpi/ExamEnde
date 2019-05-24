b2e9c3c00058a782887d2382d6ebce2df419189c
Adrian Arpi <arpiadrian5@gmail.com>
2019-05-24 13:19:27 +0200

    Version inicial de clase Casa-

---
+++ GitExamen/src/clasesExamen/Casa.java
@@ -0,0 +1,21 @@
+package clasesExamen;
+
+public class Casa {
+
+	private float superficie;
+	private String calle;
+	private int codigoPostal;
+	
+	/**
+	 * Un equipo de limpiadores deja la casa libre de gérmenes
+	 * y desinfecta todas las superficies
+	 */
+	
+	public void limpiarLaCasa(){
+		
+		String mensajito="Te hemos limpiado la casa, está desinfectada";
+		System.out.println(mensajito);
+	}
+	
+	
+}
